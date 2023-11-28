bf8ee0308bee9a50602dd89495684f2e516a6da1
Donald_Trumpyyy <demaupeoucorentin@gmail.com>
2023-11-28 19:27:23 +0100

    v1

--- app/page.js
+++ app/page.js
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import React, { useState, useEffect } from 'react';
+
+import data from "../data.json";
+
+const Flashcard = () => {
+  const [currentWord, setCurrentWord] = useState(null);
+  const [options, setOptions] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState('');
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+
+  // Function to select a random element from an array
+  const getRandomElement = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  // Function to generate a set of unique indices
+  const generateUniqueIndices = (excludeIndex) => {
+    let indices = new Set();
+    while (indices.size < 2) {
+      let randomIndex = Math.floor(Math.random() * data.length);
+      if (randomIndex !== excludeIndex) {
+        indices.add(randomIndex);
+      }
+    }
+    return [...indices];
+  };
+
+  // Function to select a word and generate options
+  const generateFlashcardData = () => {
+    // Select a random word
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const wordPair = data[randomIndex];
+
+    // Decide whether to show German or French word
+    const showGerman = Math.random() < 0.5;
+    const questionWord = showGerman ? wordPair.de : wordPair.fr;
+    const correctAnswer = showGerman ? wordPair.fr : wordPair.de;
+
+    // Generate incorrect options
+    const incorrectIndices = generateUniqueIndices(randomIndex);
+    const incorrectOptions = incorrectIndices.map(index => showGerman ? data[index].fr : data[index].de);
+
+    // Combine correct and incorrect options and shuffle
+    const options = [correctAnswer, ...incorrectOptions].sort(() => Math.random() - 0.5);
+
+    // Update state
+    setCurrentWord(questionWord);
+    setOptions(options);
+    setCorrectAnswer(correctAnswer);
+  };
+
+  useEffect(() => {
+    generateFlashcardData();
+  }, [data]);
+
+
+  const checkAnswer = (selectedAnswer) => {
+    const isCorrect = selectedAnswer === correctAnswer;
+    setIsAnswerCorrect(isCorrect);
+    setShowPopup(true);
+
+    // Optionally, hide the popup after a few seconds
+    setTimeout(() => {
+      setShowPopup(false);
+      generateFlashcardData(); // For the next question
+    }, 3000); // 3 seconds
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white shadow-lg rounded-lg">
+        <h1 className="text-xl font-bold mb-4">Flashcard</h1>
+        <p className="text-lg font-semibold mb-6">{currentWord}</p>
+        <div className="grid grid-cols-1 gap-4">
+          {options.map((option, index) => (
+            <button
+              key={index}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => checkAnswer(option)}
+            >
+              {option}
+            </button>
+          ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {showPopup && (
+        <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 border rounded shadow-lg ${isAnswerCorrect ? 'bg-green-200 border-green-400' : 'bg-red-200 border-red-400'}`}>
+          {isAnswerCorrect ? 'Correct answer!' : 'Incorrect answer. Try again!'}
+        </div>
+      )}
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Flashcard;
