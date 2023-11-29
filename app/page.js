020442e2a25d863d2bc51dcd90dbe95dabaaf421
Donald_Trumpyyy <demaupeoucorentin@gmail.com>
2023-11-29 09:09:05 +0100

    adding input feature

--- app/page.js
+++ app/page.js
@@ -5,12 +5,18 @@ import React, { useState, useEffect } from 'react';
 import data from "../data.json";
 
 const Flashcard = () => {
+  // Existing states
   const [currentWord, setCurrentWord] = useState(null);
   const [options, setOptions] = useState([]);
   const [correctAnswer, setCorrectAnswer] = useState('');
-
   const [showPopup, setShowPopup] = useState(false);
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+
+  // New state for toggling between options and input
+  const [showOptions, setShowOptions] = useState(true);
+
+  // New state for user input
+  const [userInput, setUserInput] = useState('');
 
   // Function to select a random element from an array
   const getRandomElement = (array) => {
@@ -70,23 +76,60 @@ const Flashcard = () => {
     }, 3000); // 3 seconds
   };
 
+  const handleSubmit = () => {
+    const isCorrect = userInput.trim() === correctAnswer;
+    setIsAnswerCorrect(isCorrect);
+    setShowPopup(true);
+
+    setTimeout(() => {
+      setShowPopup(false);
+      generateFlashcardData(); // For the next question
+      setUserInput(''); // Reset the input field
+    }, 3000); // 3 seconds
+  };
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="p-6 bg-white shadow-lg rounded-lg">
         <h1 className="text-xl font-bold mb-4">Flashcard</h1>
         <p className="text-lg font-semibold mb-6">{currentWord}</p>
-        <div className="grid grid-cols-1 gap-4">
-          {options.map((option, index) => (
+
+        <button
+          onClick={() => setShowOptions(!showOptions)}
+          className="mb-4 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+        >
+          {showOptions ? "Switch to Input" : "Switch to Options"}
+        </button>
+
+        {showOptions ? (
+          <div className="grid grid-cols-1 gap-4">
+            {options.map((option, index) => (
+              <button
+                key={index}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => checkAnswer(option)}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <input
+              type="text"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              className="border-2 border-gray-300 rounded p-2 mr-2"
+            />
             <button
-              key={index}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => checkAnswer(option)}
+              onClick={handleSubmit}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
             >
-              {option}
+              Submit
             </button>
-          ))}
-        </div>
+          </div>
+        )}
       </div>
 
       {showPopup && (
